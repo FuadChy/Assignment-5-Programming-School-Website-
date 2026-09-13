3ede9df8c0b3ffa5fd376f3351ffa9a3cc8693d2
FuadChy <fuadchy2020@gmail.com>
2026-09-13 09:38:17 +0600

    card section final design

--- src/Component/Technologies/Technologies.tsx
+++ src/Component/Technologies/Technologies.tsx
@@ -8,7 +8,7 @@ const Technologies = () => {
             {/* central div */}
             <div className="container mx-auto max-w-6xl px-4">
                 {/* explore part upper div */}
-                <div className="mb-8 text-center">
+                <div className="mb-8 text-left">
                     <h2 className="text-3xl font-bold text-gray-900">Explore the <span className="bg-linear-to-r from-orange-500 via-pink-500 to-violet-600 bg-clip-text text-transparent">Technologies</span></h2>
             <p>Pick one technology per category to build your ideal stack.</p>
                 </div>
@@ -18,21 +18,28 @@ const Technologies = () => {
 technologies.map((technology) => (
 // individual card div
   <div key={technology.id} className="flex h-full flex-col rounded-xl border border-gray-200 bg-white p-5 shadow-sm">
-   <img
-      src={technology.icon}
-      alt={technology.name}
-      className="h-12 w-12"
-    />
-    <h3 className="text-xl font-semibold">{technology.name}</h3>
-    <span className="rounded-full bg-pink-100 px-3 py-1 text-sm text-pink-600">
-  {technology.badge}
-</span>
+   <div className="flex items-center gap-4">
+  <img
+    src={technology.icon}
+    alt={technology.name}
+    className="h-12 w-12"
+  />
+
+  <h3 className="text-xl font-semibold">
+    {technology.name}
+  </h3>
+</div>
+<div className="mt-2">
+  <span className="rounded-full bg-pink-100 px-3 py-1 text-sm text-pink-600">
+    {technology.badge}
+  </span>
+</div>
 <p className="mt-3 text-gray-600">
   {technology.description}
 </p>
 <div className="mt-4 flex items-center justify-between">
-  <span>{technology.category}</span>
-  <span>{technology.difficulty}</span>
+  <span  className="rounded-full bg-gray-100 px-3 py-1 text-sm text-gray-600">{technology.category}</span>
+  <span className="text-sm text-gray-500">{technology.difficulty}</span>
 </div>
 <div className="mt-4">
   <span>⭐</span>
